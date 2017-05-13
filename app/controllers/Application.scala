1443e192775c7164d34b3fb6521d49f9f6655fd4
Oleksiy Blavat <blavat.alex@gmail.com>
2017-05-13 15:25:31 -0400

    practice with layouts

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,17 @@ import play.api.mvc._
 class Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    val time = new java.util.Date();
+    val names = List("James", "Oliver", "PopEye", "Gordon", "Nathan")
+    val names2 = List.empty
+    Ok(views.html.index("Hello, again, world - passed from Controller <small>SMALL FONT</small>", names2, time.toString))
+  }
+
+  def another = Action { implicit request =>
+    val initialValue = request.session.get("counter").map(_.toInt).getOrElse(0)
+    Ok(views.html.another()).withSession(
+      "counter" -> (initialValue + 1).toString
+    )
   }
 
 }
